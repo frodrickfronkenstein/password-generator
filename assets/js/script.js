28eae19d04711a9b16f554195a496a6b20e47409
Eric Smith <ericandrewsmith88@gmail.com>
2022-03-12 22:44:08 -0500

    finished password length prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,29 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // Function to generate password
-function generatePassword(length = 8) {
+function generatePassword(length) {
+  // prompts user to determine length of password
+  var length = prompt("how many characters long do you need your password to be? min. 8, max 128");
+  if (length === "" || null) {
+    alert("kinda need an answer on this one");
+    return generatePassword(length);
+  }
+  length = parseInt(length);
+
+  if (length < 8) {
+    alert("password needs to be longer. min. 8 characters");
+    return generatePassword(length);
+  }
+
+  if (length > 128) {
+    alert("password is too long. max. 128 characters");
+    return generatePassword(length);
+  }
+ 
   var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "1234567890";
+  var specialCharacters = "~`!@#$%^&*()-_=+{[}]\|;:',<.>/?"
    
   var pword =  new Array ();
 
